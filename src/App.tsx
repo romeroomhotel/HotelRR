d946b5198fc6ab24d94db895bf2b8c3329992082
Er Leo <romeroom.hotel@gmail.com>
2023-04-07 05:05:03 +0200

    clean + add gfont&FA

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>A NEW START</h1>
+      <h1> A NEW START </h1>
     </div>
   );
 }
