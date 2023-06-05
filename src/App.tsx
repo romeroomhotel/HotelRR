33ba5d80f283e55458b4c306840819a52ca93927
Er Leo <romeroom.hotel@gmail.com>
2023-06-05 16:32:16 +0200

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
 import Hotelinfo from './pages/HotelInfoPage/Hotelinfo';
@@ -6,12 +5,14 @@ import RoomsPage from './pages/RoomsPage/RoomsPage';
 import InfoUtilsPage from './pages/InfoUtilsPage/InfoUtilPage';
 import NearPlacesPage from './pages/AroundPage/AroundPage';
 import { ParallaxProvider } from 'react-scroll-parallax';
+import ScrollaTop from './components/ScrollaTop/ScrollaTop';
 
 const App = () => {
 
   return (
     <ParallaxProvider>
       <BrowserRouter>
+        <ScrollaTop />
         <Routes>
           <Route path='/' element={<Homepage />} />
           <Route path='/Hotelinfo' element={<Hotelinfo />} />
